4ff5a919cf3f05ae9b59b98047d2506e2a167bbe
krishnasrihari <krishna.srihari@gmail.com>
2013-01-29 18:23:01 +0530

    jquery migrate rails init

---
+++ lib/jquery-migrate-rails.rb
@@ -0,0 +1,9 @@
+require "jquery-migrate-rails/version"
+
+module Jquery
+  module Migrate
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jquery-migrate-rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Migrate
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
